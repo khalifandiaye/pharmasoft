028227f101ab17dad00534203157e8544c8e53f2
moustaphamb@gmail.com <moustaphamb@gmail.com@11cddf64-6fb3-b167-68fa-693e53c0ab82>
2013-10-31 16:40:59 +0000

    dao et fenetre typeMedicament

---
+++ trunk/pharmaSoft/src/main/java/fr/mmm/pharmaSoft/dao/Daos.java
@@ -0,0 +1,66 @@
+package fr.mmm.pharmaSoft.dao;
+
+public class Daos {
+	
+	private CommandeDao commandeDao;
+	
+	private TypeMedicamentDao typeMedicamentDao;
+	
+	private EmployeDao employeDao;
+	
+	private StockDao stockDao;
+	
+	private MedicamentDao medicamentDao;
+
+	public Daos() {
+		super();
+		
+	}
+
+	public CommandeDao getCommandeDao() {
+		return commandeDao;
+	}
+
+	public void setCommandeDao(CommandeDao commandeDao) {
+		this.commandeDao = commandeDao;
+	}
+
+	public TypeMedicamentDao getTypeMedicamentDao() {
+		return typeMedicamentDao;
+	}
+
+	public void setTypeMedicamentDao(TypeMedicamentDao typeMedicamentDao) {
+		this.typeMedicamentDao = typeMedicamentDao;
+	}
+
+	public EmployeDao getEmployeDao() {
+		return employeDao;
+	}
+
+	public void setEmployeDao(EmployeDao employeDao) {
+		this.employeDao = employeDao;
+	}
+
+	public StockDao getStockDao() {
+		return stockDao;
+	}
+
+	public void setStockDao(StockDao stockDao) {
+		this.stockDao = stockDao;
+	}
+
+	public MedicamentDao getMedicamentDao() {
+		return medicamentDao;
+	}
+
+	public void setMedicamentDao(MedicamentDao medicamentDao) {
+		this.medicamentDao = medicamentDao;
+	}
+	
+	
+	
+	
+	
+	
+
+}
